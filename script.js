6d0cac800c2fb56a744479c0ac3d0e4a4fa273cd
Avishek <kumaravishek84@gmail.com>
2021-06-19 23:44:20 +0530

    logo added

--- script.js
+++ script.js
@@ -1,17 +1,40 @@
 let inpNewItem = document.getElementById("inpNewItem");
 let btnClear = document.getElementById("btnClear");
 let listGroup = document.getElementsByClassName("list-group")[0];
+let btnAdd = document.getElementById("btnAdd");
 
 btnAdd.addEventListener("click", function () {
     let inputByUser = inpNewItem.value;
     if (inputByUser) {
         let newListItem = document.createElement("li");
         newListItem.className = "list-group-item";
-        newListItem.innerHTML=`<i class="far fa-check-circle"></i>`;
+
         newListItem.innerHTML += inputByUser;
+
+        let f = 0;
+        newListItem.addEventListener("click", function () {
+            if (f == 0) {
+                newListItem.style.textDecoration = "line-through";
+                f = 1;
+            }
+            else {
+                newListItem.style.textDecoration = "none";
+                f = 0;
+            }
+        });
+        let delBtn = `<i class="fas fa-trash-alt" id="delBtn"></i>`;
+
+        newListItem.innerHTML += delBtn;
+        delBtn = newListItem.childNodes[1];
         listGroup.appendChild(newListItem);
+        delBtn.addEventListener("click", function () {
+            listGroup.removeChild(newListItem);
+        });
         inpNewItem.value = "";
-        newListItem.innerHTML+=`<i class="fas fa-trash-alt"></i>`;
+
+    }
+    else {
+        alert("Enter something");
     }
 
 });
